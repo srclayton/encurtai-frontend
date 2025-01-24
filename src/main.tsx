4ec57d3ec7fe89a9cac1ab8725599068a3427b26
Clayton <clayton@srocha.dev>
2025-01-24 13:41:55 -0300

    Remove unused CSS dependency and add local styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-import "srocha_skeleton_css/css/normalize.css";
-import "srocha_skeleton_css/css/skeleton.css";
+import "./css/normalize.css";
+import "./css/skeleton.css";
 import "./main.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -13,31 +13,31 @@ import Contact from "./routes/contact/page.tsx";
 import Header from "./components/Header/Header.tsx";
 import Footer from "./components/Footer/Footer.tsx";
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/sobre",
-    element: <About />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/contato",
-    element: <Contact />,
-  },
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <NotFound />,
+	},
+	{
+		path: "/sobre",
+		element: <About />,
+		errorElement: <NotFound />,
+	},
+	{
+		path: "/contato",
+		element: <Contact />,
+	},
 ]);
 const isPathValid = !!(
-  window.location.pathname === "/" ||
-  window.location.pathname === "/sobre" ||
-  window.location.pathname === "/contato"
+	window.location.pathname === "/" ||
+	window.location.pathname === "/sobre" ||
+	window.location.pathname === "/contato"
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    {isPathValid && <Header />}
-    <RouterProvider router={router} />
-    {isPathValid && <Footer />}
-  </React.StrictMode>,
+	<React.StrictMode>
+		{isPathValid && <Header />}
+		<RouterProvider router={router} />
+		{isPathValid && <Footer />}
+	</React.StrictMode>
 );
